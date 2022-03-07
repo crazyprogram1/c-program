a44a51d087bed6f013737b8bd40f27f676e4f92d
crazyprogram1 <100140532+crazyprogram1@users.noreply.github.com>
2022-03-07 14:51:34 +0800

    第五天c语言练习

---
+++ test5.c
@@ -0,0 +1,233 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//作业题目九个作业
+//1.3个数从大到小输出
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		int tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//	printf("%d %d %d\n",a,b,c);
+//	return 0;	
+//}
+//作业二
+//写一个代码打印1-100之间所有的3倍数的数字
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 3 == 0)
+//			printf("%d", i);
+//	}
+//}
+//作业三求两个数的最大公约数
+//#include<stdio.h>
+//int main()
+//{
+//	int m = 24;
+//	int n = 18;
+//	int r = 0;
+//	scanf("%d%d", &m, &n);
+//	while (r=m % n)
+//	{
+//	/*	r = m % n;*/
+//		m = n;
+//		n = r;
+//	}
+//	printf("%d\n", n);
+//}
+//作业四打印闰年打印1000到2000之间的
+// 1.可以被4整整除，不可以被100整除
+// 2.可以被400整除
+//#include<stdio.h>
+//int main()
+//{
+//	int year = 0;
+//	int count = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		//{
+//		//	if (year % 4 == 0 && year % 100 != 0)
+//		//	{
+//		//		printf("%d ", year);
+//		//		count++;
+//		//	}
+//		//	else if (year % 400 == 0)
+//		//	{
+//		//		printf("%d ", year);
+//		//		count++;
+//		//	}
+//		//}
+//		if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+//		{
+//			printf("%d", year);
+//			count++;
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//}
+//作业五打印100到200的素数
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断i是否是素数1.试除法 产生2到i-1的数字去除于n
+//		int j = 0;
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j == i)
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//		return 0;
+//}
+//法二如果有i=a*b a或者b中至少有一个数字<=开平方i 
+//#include<math.h>
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断i是否是素数1.试除法 产生2到i-1的数字去除于n
+//		int j = 0;
+//		for (j = 2; j < sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j>sqrt(i))
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//		return 0;
+//}
+//作业六1到100中出现多少个数字9
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int count=0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 == 9)
+//			count++;
+//		if (i / 10 == 9)
+//			count++;
+//	}
+//	printf("count=%d\n ",count);
+//	return 0;
+//}
+//作业七分数求和计算1/1-1/2+1/3-1/4........+1/99-1/100的值，打印出结果
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += flag*1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+//作业八求10个整数最大值
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int max = arr[0];
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 1; i < sz; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+//作业八乘法口诀
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//作业九二分查找
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left<=right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			printf("找到了，下标是：%d\n", mid);
+//			break;
+//		}
+//	}
+//	if (left > right)
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//}//二分法
